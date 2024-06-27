ebef5cc6e8cda1a60e4f0ef160a94a30c9b363e1
DhruvSaboo <saboodhruv789@gmail.com>
2024-06-27 16:36:23 +0530

    error-resolved

--- src/components/ProjectDetails/index.jsx
+++ src/components/ProjectDetails/index.jsx
@@ -214,9 +214,6 @@ const index = ({ openModal, setOpenModal }) => {
                                     <Member>
                                         <MemberImage src={member.img} />
                                         <MemberName>{member.name}</MemberName>
-                                        <a href={member.github} target="new" style={{textDecoration: 'none', color: 'inherit'}}>
-                                            <GitHub />
-                                        </a>
                                         <a href={member.linkedin} target="new" style={{textDecoration: 'none', color: 'inherit'}}>
                                             <LinkedIn />
                                         </a>
